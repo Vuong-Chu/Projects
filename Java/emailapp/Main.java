98bb301bbf5df14097c0cae9b836243ecb1ff987
Vuong-Chu <42327886+Vuong-Chu@users.noreply.github.com>
2021-03-03 18:19:50 +0000

    Add files via upload

--- Java/emailapp/Main.java
+++ Java/emailapp/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
-        Parent root = FXMLLoader.load(getClass().getResource("/emailapp/App.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/emailapp/view.fxml"));
         Scene scene = new Scene(root);
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
